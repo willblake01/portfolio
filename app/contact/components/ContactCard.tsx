3f3e2105b2375de71fc6fa104db7c03abf30f712
Will Blake <willblakebooking@gmail.com>
2024-12-27 22:48:49 -0600

    Remove contact card picture

--- app/contact/components/ContactCard.tsx
+++ app/contact/components/ContactCard.tsx
@@ -3,14 +3,6 @@ import Link from 'next/link'
 
 const ContactCard = () => (
   <Card sx={{ borderRadius: 2, height: '800ox', width: '37.5rem' }}>
-    <CardMedia
-      sx={{
-        height: 440,
-        marginBottom: 4,
-      }}
-      image="https://res.cloudinary.com/willblake01/image/upload/v1538510164/react-portfolio/landing-background.jpg"
-      title="Contact"
-    />
     <CardContent>
       <Typography component={'p'}>Email: willblakebooking@gmail.com</Typography>
       <Typography component={'p'}>
