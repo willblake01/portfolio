ab17f1e689ce99926c35bfd7a481e1cf0c555e34
William Blake <willblakebooking@gmail.com>
2026-01-16 13:16:43 -0600

    Add Vercel analytics and speed-insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import localFont from 'next/font/local'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -20,6 +22,8 @@ const RootLayout = ({
   <html lang="en">
     <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
       {children}
+      <Analytics />
+      <SpeedInsights />
     </body>
   </html>
 )
